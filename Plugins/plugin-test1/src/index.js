03535b2385bced9f24c8a8ca7ad8bd3624d9f4e1
Prasanth Pillai <ppillai@twilio.com>
2021-07-29 18:27:05 +1000

    intial commit

---
+++ Plugins/plugin-test1/src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import Test1Plugin from './Test1Plugin';
+
+FlexPlugin.loadPlugin(Test1Plugin);
---
+++ Plugins/plugin-test2/src/index.js
@@ -0,0 +1,4 @@
+import * as FlexPlugin from 'flex-plugin';
+import Test2Plugin from './Test2Plugin';
+
+FlexPlugin.loadPlugin(Test2Plugin);
