d7a04fcd898e88eea5375e4ce41bfca86979fdf3
Prasanth Pillai <ppillai@twilio.com>
2021-08-01 12:28:22 +1000

    removed

--- Flex/plugin-alerttask/README.md
+++
@@ -1,5 +0,0 @@
-# plugin-alert-task
-
-Plays the cowbell.mp3 tune when a reservation is created
-
-Enrolled into PS platform
--- Flex/plugin-alerttask/src/index.js
+++
@@ -1,4 +0,0 @@
-import * as FlexPlugin from 'flex-plugin';
-import AlertTaskPlugin from './AlertTaskPlugin';
-
-FlexPlugin.loadPlugin(AlertTaskPlugin);
