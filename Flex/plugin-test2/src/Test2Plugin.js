9242c4c95ac5656c8649091ea25f9371cf6c9db8
Prasanth Pillai <ppillai@twilio.com>
2021-08-03 21:39:23 +1000

    changes for plugins

--- Flex/plugin-test2/src/Test2Plugin.js
+++ Flex/plugin-test2/src/Test2Plugin.js
@@ -13,7 +13,7 @@ export default class Test2Plugin extends FlexPlugin {
   }
 
   /**
-   * This code is run when your plugin is being started - test
+   * This code is run when your plugin is being started - 
    * Use this to modify any UI components or attach to the actions framework test
    *
    * @param flex { typeof import('@twilio/flex-ui') }
